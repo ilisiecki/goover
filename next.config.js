ce0bd693054e65c045cf34ca7afd809edfc0c2d9
ilisiecki <igor.fox.lisiecki@gmail.com>
2023-10-13 00:03:42 +0100

    Create middleware and navbar for users

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (
-        config,
-        { buildId, dev, isServer, defaultLoaders, webpack }
-      ) => {
-        config.resolve.alias.canvas = false
-        config.resolve.alias.encoding = false
-        return config
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "**",
       },
-}
+    ],
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -3,10 +3,18 @@ import Link from "next/link";
 import { MaxWidthWrapper } from "./max-width-wrapper";
 import { buttonVariants } from "../ui/button";
 
-import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+  RegisterLink,
+  LoginLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 import { ArrowRightIcon } from "lucide-react";
+import UserAccountNav from "./user-account-nav";
 
 export function Navbar() {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
   return (
     <nav className="sticky inset-x-0 top-0 z-30 h-14 w-full border-b border-neutral-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
@@ -14,25 +22,44 @@ export function Navbar() {
           <Link href="/" className="z-40 flex font-semibold">
             <span>Goover</span>
           </Link>
-          {/* todo: add mobile navbar */}
           <div className="hidden items-center space-x-4 sm:flex">
-            <>
-              <Link
-                href="/pricing"
-                className={buttonVariants({ size: "sm", variant: "ghost" })}
-              >
-                Pricing
-              </Link>
-              <LoginLink
-                className={buttonVariants({ size: "sm", variant: "ghost" })}
-              >
-                Sign in
-              </LoginLink>
-              <RegisterLink className={buttonVariants({ size: "sm" })}>
-                <span className="pr-1.5">Get started</span>{" "}
-                <ArrowRightIcon className="h-5 w-5" />
-              </RegisterLink>
-            </>
+            {!user ? (
+              <>
+                <Link
+                  href="/pricing"
+                  className={buttonVariants({ size: "sm", variant: "ghost" })}
+                >
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={buttonVariants({ size: "sm", variant: "ghost" })}
+                >
+                  Sign in
+                </LoginLink>
+                <RegisterLink className={buttonVariants({ size: "sm" })}>
+                  <span className="pr-1.5">Get started</span>{" "}
+                  <ArrowRightIcon className="h-5 w-5" />
+                </RegisterLink>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/pricing"
+                  className={buttonVariants({ size: "sm", variant: "ghost" })}
+                >
+                  Pricing
+                </Link>
+                <UserAccountNav
+                  name={
+                    !user.given_name || !user.family_name
+                      ? "Your Account"
+                      : `${user.given_name} ${user.family_name}`
+                  }
+                  email={user.email ?? ""}
+                  imageUrl={user.picture ?? ""}
+                />
+              </>
+            )}
           </div>
         </div>
       </MaxWidthWrapper>
